95a092b870488e1cb5a1a36807f65aefefca9325
samedberry <143440194+samedberry@users.noreply.github.com>
2023-09-25 12:57:57 -0600

    Exam1 update 3
    Finished TenMaker

--- src/TenMaker.java
+++ src/TenMaker.java
@@ -1,4 +1,20 @@
 import java.util.Scanner;
 
 public class TenMaker {
+    public static void main(String[] args) {
+        //define necessaries
+        Scanner input1 = new Scanner(System.in);
+
+        System.out.println("Please enter an integer: ");
+        int num1 = input1.nextInt();
+
+        System.out.println("Please enter a second integer: ");
+        int num2 = input1.nextInt();
+
+        if (num1+num2 == 10){
+            System.out.println("The sum of these integers is 10.");
+        } else {
+            System.out.println("The sum of these integers is not 10.");
+        }
+    }
 }
